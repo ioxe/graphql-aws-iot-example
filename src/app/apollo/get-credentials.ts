cee7f8c50db1f922a47bf49c0ff18354d534796e
Girish Nanda <girishnanda@gmail.com>
2017-09-07 09:24:06 -0700

    remove cognito workaround

--- src/app/apollo/get-credentials.ts
+++ src/app/apollo/get-credentials.ts
@@ -9,11 +9,11 @@ import { Credentials } from 'aws-sdk/global';
 export const getCredentials = () => {
     // cognito specific workaround for this demo to support multiple tabs in the same browser (potentially there is a better one)
     // clientId must be unique per tab. If localstorage is not cleared - the second tab will try to reuse the same identity.
-    for (const key in localStorage) {
-        if (key.indexOf('aws.cognito.') !== -1) {
-            localStorage.removeItem(key);
-        }
-    }
+    // for (const key in localStorage) {
+    //     if (key.indexOf('aws.cognito.') !== -1) {
+    //         localStorage.removeItem(key);
+    //     }
+    // }
     const credentials = new CognitoIdentityCredentials({
         IdentityPoolId: environment.identityPoolId,
     });
